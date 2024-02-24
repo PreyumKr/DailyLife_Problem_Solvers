eeb6c887ec9bcef2f783529a58fbe1b1686a07b3
Preyum Kumar <preyumkrsingh@gmail.com>
2024-02-24 19:53:37 +0530

    Updated Scripts

---
+++ Multiple Files Rename/Multi_Remove_TailingText_file.py
@@ -0,0 +1,18 @@
+import os
+
+root = os.getcwd()
+
+for file in os.listdir('.'):
+
+  if not os.path.isfile(file):
+    continue
+
+  head, tail = os.path.splitext(file)
+   
+  # Specify the extension of files you trying to edit at once
+  if tail == ".jpg":
+    src = os.path.join(root, file)
+    dst = os.path.join(root, file.replace("_Trailingtext", ""))
+  
+  if not os.path.exists(dst): # check if the file doesn't exist
+    os.rename(src, dst)  
